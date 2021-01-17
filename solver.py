d7ad8d8e50edef82cc9ba350e8aab555a42a0cc1
David <davhofer@ethz.ch>
2021-01-17 14:51:01 +0100

    a

--- solver.py
+++ solver.py
@@ -1,13 +1,7 @@
 
-def deepcopy(grid):
-    grid2 = [x.copy() for x in grid]
-    return grid2
-
-
 # get the number of the tile that position i,j belong to
 def tile(i,j):
     return (i//3)*3+j//3
-
 
 
 # get the next index of the grid, row-major order
@@ -21,7 +15,6 @@ def next(i,j):
 class Grid:
     def __init__(self,setup_grid):
         # hash sets for every row, col and tile
-        self.defaultGrid = setup_grid
         self.rows = [set() for i in range(9)]
         self.cols = [set() for i in range(9)]
         self.tiles = [set() for i in range(9)]
@@ -36,38 +29,11 @@ class Grid:
                     self.tiles[tile(i,j)].add(el)
                     self.defaults.add((i,j))
 
-        self.grid = deepcopy(setup_grid)
-
-    # set grid back to initial state
-    def restoreDefault(self):
-        self.grid = deepcopy(self.defaultGrid)
+        self.grid = setup_grid
 
     # check if a number is currently allowed at position i,j
     def check(self,num,i,j):
         return num not in self.rows[i] and num not in self.cols[j] and num not in self.tiles[tile(i,j)]
-
-    # change value at i,j to num
-    def put(self,i,j,num):
-        self.remove(i,j)
-        self.add(i,j,num)
-
-    # add number at pos i,j
-    def add(self,num,i,j):
-        self.rows[i].add(num)
-        self.cols[j].add(num)
-        self.tiles[tile(i,j)].add(num)
-        self.grid[i][j] = num
-
-    # remove num at i,j
-    def remove(self,i,j):
-        num = self.grid[i][j]
-        if num in self.rows[i]:
-            self.rows[i].remove(num)
-        if num in self.cols[j]:
-            self.cols[j].remove(num)
-        if num in self.tiles[tile(i,j)]:
-            self.tiles[tile(i,j)].remove(num)
-        self.grid[i][j] = 0
 
     # recursively fill in the sudoku, starting at position i,j
     def solveFrom(self,i,j):
@@ -119,7 +85,7 @@ class Grid:
             print("This sudoku has no solution!")
 
 
-grid1 = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+test_grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
           [5, 2, 0, 0, 0, 0, 0, 0, 0],
           [0, 8, 7, 0, 0, 0, 0, 3, 1],
           [0, 0, 3, 0, 1, 0, 0, 8, 0],
@@ -129,19 +95,8 @@ grid1 = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
           [0, 0, 0, 0, 0, 0, 0, 7, 4],
           [0, 0, 5, 2, 0, 6, 3, 0, 0]]
 
-grid2 =[
-    [7, 8, 0, 4, 0, 0, 1, 2, 0],
-    [6, 0, 0, 0, 7, 5, 0, 0, 9],
-    [0, 0, 0, 6, 0, 1, 0, 7, 8],
-    [0, 0, 7, 0, 4, 0, 2, 6, 0],
-    [0, 0, 1, 0, 5, 0, 9, 3, 0],
-    [9, 0, 4, 0, 6, 0, 0, 0, 5],
-    [0, 7, 0, 3, 0, 0, 0, 1, 2],
-    [1, 2, 0, 0, 0, 7, 4, 0, 0],
-    [0, 4, 9, 2, 0, 6, 0, 0, 7]
-]
 
-g = Grid(grid1)
+g = Grid(test_grid)
 
 if __name__ == "__main__":
     g.solve()
